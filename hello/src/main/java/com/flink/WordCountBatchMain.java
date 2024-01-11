db94857588af9249e7f146806516eceefc0f4bd7
hcx0081 <623363564@qq.com>
2024-01-11 18:06:09 +0800

    首次提交

---
+++ hello/src/main/java/com/flink/WordCountBatchMain.java
@@ -0,0 +1,36 @@
+package com.flink;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.AggregateOperator;
+import org.apache.flink.api.java.operators.DataSource;
+import org.apache.flink.api.java.operators.FlatMapOperator;
+import org.apache.flink.api.java.operators.UnsortedGrouping;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.util.Collector;
+
+/**
+ * {@code @description:} DataSet API 实现 WordCount
+ */
+public class WordCountBatchMain {
+    public static void main(String[] args) throws Exception {
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        DataSource<String> ds = env.readTextFile("data/word");
+        FlatMapOperator<String, Tuple2<String, Integer>> wordOne = ds.flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
+            @Override
+            public void flatMap(String value, Collector<Tuple2<String, Integer>> collector) throws Exception {
+                String[] words = value.split(" ");
+                for (String word : words) {
+                    Tuple2<String, Integer> wordTuple2 = Tuple2.of(word, 1);
+                    collector.collect(wordTuple2);
+                }
+            }
+        });
+        // 按照word分组
+        UnsortedGrouping<Tuple2<String, Integer>> wordOneGroupBy = wordOne.groupBy(0);
+        // 各分组内聚合
+        AggregateOperator<Tuple2<String, Integer>> sum = wordOneGroupBy.sum(1);
+        // 输出
+        sum.print();
+    }
+}
---
+++ hello/src/main/java/com/flink/WordCountStreamMain.java
@@ -0,0 +1,44 @@
+package com.flink;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.KeyedStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
+/**
+ * {@code @description:} DataStream API 实现 WordCount
+ */
+public class WordCountStreamMain {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        DataStreamSource<String> ds = env.readTextFile("data/word");
+        SingleOutputStreamOperator<Tuple2<String, Integer>> wordOne = ds.flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
+            @Override
+            public void flatMap(String value, Collector<Tuple2<String, Integer>> collector) throws Exception {
+                String[] words = value.split(" ");
+                for (String word : words) {
+                    Tuple2<String, Integer> wordTuple2 = Tuple2.of(word, 1);
+                    collector.collect(wordTuple2);
+                }
+            }
+        });
+        // 按照word分组
+        KeyedStream<Tuple2<String, Integer>, String> wordOneKeyBy = wordOne.keyBy(new KeySelector<Tuple2<String, Integer>, String>() {
+            @Override
+            public String getKey(Tuple2<String, Integer> value) throws Exception {
+                return value.f0;
+            }
+        });
+        // 各分组内聚合
+        SingleOutputStreamOperator<Tuple2<String, Integer>> sum = wordOneKeyBy.sum(1);
+        // 输出
+        sum.print();
+        
+        // 执行（必须调用）
+        env.execute();
+    }
+}
