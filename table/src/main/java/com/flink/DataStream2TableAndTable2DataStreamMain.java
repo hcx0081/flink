4f3df29bbbb1645200ed95f69cb1b7b4fb12f643
hcx <623363564@qq.com>
2024-06-05 15:02:19 +0800

    测试一下

--- table/src/main/java/com/flink/DataStream2TableAndTable2DataStreamMain.java
+++ table/src/main/java/com/flink/DataStream2TableAndTable2DataStreamMain.java
@@ -1,10 +1,19 @@
 package com.flink;
 
 import com.flink.bean.WaterSensor;
+import org.apache.flink.connector.jdbc.JdbcConnectionOptions;
+import org.apache.flink.connector.jdbc.JdbcSink;
+import org.apache.flink.connector.jdbc.JdbcStatementBuilder;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.Table;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+import org.apache.flink.types.Row;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Set;
 
 /**
  * {@code @description:}
@@ -12,6 +21,7 @@ import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 public class DataStream2TableAndTable2DataStreamMain {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        StreamTableEnvironment tEnv = StreamTableEnvironment.create(env);
         
         DataStreamSource<WaterSensor> dataStream = env.fromElements(
                 new WaterSensor("1", 1L, 1),
@@ -19,17 +29,40 @@ public class DataStream2TableAndTable2DataStreamMain {
                 new WaterSensor("3", 3L, 3)
         );
         
-        StreamTableEnvironment tEnv = StreamTableEnvironment.create(env);
-        
         // 流转表
         Table waterSensorTable = tEnv.fromDataStream(dataStream);
         tEnv.createTemporaryView("water_sensor", waterSensorTable);
         Table table = tEnv.sqlQuery("select * from water_sensor");
-        table.execute().print();
+        // table.execute().print();
         
         // 表转流
         // DataStream<WaterSensor> waterSensorDataStream = tEnv.toDataStream(table, WaterSensor.class);
         // waterSensorDataStream.print();
         // env.execute();
+        
+        
+        DataStream<Row> ds = tEnv.toDataStream(table);
+        ds.addSink(JdbcSink.sink(
+                "insert into water_sensor(*) values(?,?,?)",
+                new JdbcStatementBuilder<Row>() {
+                    @Override
+                    public void accept(PreparedStatement preparedStatement, Row row) throws SQLException {
+                        row = Row.withNames();
+                        Set<String> fieldNames1 = row.getFieldNames(false);
+                        Set<String> fieldNames = row.getFieldNames(true);
+                        System.out.println(fieldNames1);
+                        System.out.println(fieldNames);
+                        System.out.println(row);
+                    }
+                },
+                new JdbcConnectionOptions.JdbcConnectionOptionsBuilder()
+                        .withUrl("jdbc:mysql://10.0.70.225:4000/bigdata")
+                        .withUsername("root")
+                        .withPassword("tiDB57@0523#")
+                        .build()
+        ));
+        
+        
+        env.execute();
     }
 }
